c6d49c627fa1a72b2d5cf3d84d20fa3cf699dfc1
Jason <theniteowl@gmail.com>
2022-06-16 21:40:35 -0700

    -got busted AF roll working, does not orient roll to player direction :S

--- Player.gd
+++ Player.gd
@@ -7,6 +7,8 @@ const MAX_SLOPE_ANGLE = 100
 
 var vel = Vector3()
 var dir = Vector3()
+var roll_right = false
+var roll_left = false
 var camera
 var rotation_helper
 
@@ -22,9 +24,13 @@ func _physics_process(delta):
 	process_input(delta)
 	process_movement(delta)
 	
+	
 func process_input(delta):
-	# walking
+
 	dir = Vector3()
+	roll_right = false
+	roll_left = false
+
 	var cam_xform = camera.get_global_transform()
 	var input_movement_vector = Vector3()
 
@@ -41,6 +47,11 @@ func process_input(delta):
 		input_movement_vector.z +=1
 	if Input.is_action_pressed("movement_descend"):
 		input_movement_vector.z -=1
+
+	if Input.is_action_pressed("movement_roll_right"):
+		roll_right = true
+	if Input.is_action_pressed("movement_roll_left"):
+		roll_left = true
 
 	input_movement_vector = input_movement_vector.normalized()
 
@@ -66,6 +77,11 @@ func process_movement(delta):
 	newVel = newVel.linear_interpolate(target, calculate_acceleration(newVel) * delta)
 	
 	vel = move_and_slide(newVel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
+	
+	if(roll_left):
+		rotate(Vector3(0, 0, 1), -.05)
+	if(roll_right):
+		rotate(Vector3(0, 0, 1), .05)
 	
 func calculate_acceleration(newVel):
 	if dir.dot(newVel) > 0:
