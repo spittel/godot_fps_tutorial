4c7257524e148bdc61bcd4b7b0051a82c779a7b0
Jason <theniteowl@gmail.com>
2022-06-18 20:46:00 -0700

    - old control cleanup - saggital movement

--- Player.gd
+++ Player.gd
@@ -18,21 +18,15 @@ var yaw_input = 0
 
 var is_drift = false
 
-var old_forward_speed = 0
 
+var horiz_speed = 0
+var vert_speed = 0
 ##################################
 # old control code
 ##################################
 var MOUSE_SENSITIVITY = 0.05
 var camera
 var rotation_helper
-#var dir = Vector3()
-var vel = Vector3()
-
-const MAX_SPEED = 30
-const ACCEL = 3
-const DEACCELL = 0.5
-const MAX_SLOPE_ANGLE = 100
 
 func _ready():
 	camera = $Rotation_Helper/Camera
@@ -48,20 +42,18 @@ func get_input(delta):
 			Input.set_mouse_mode((Input.MOUSE_MODE_VISIBLE))
 
 	# https://kidscancode.org/godot_recipes/3d/spaceship/
-	
 	if Input.is_action_pressed("throttle_up"):
 		forward_speed = lerp(forward_speed,
 			Input.get_action_strength("throttle_up")*100,
 			acceleration * delta)
 			
-#		old_forward_speed = lerp(old_forward_speed, max_speed, acceleration * delta)
 	if Input.is_action_pressed("throttle_down"):
 		forward_speed = lerp(forward_speed,
 			-Input.get_action_strength("throttle_down")*50,
 			acceleration * delta)
 			
 	is_drift = Input.is_action_pressed("drift")
-		
+	
 
 	pitch_input = lerp(pitch_input,
 			Input.get_action_strength("pitch_down") - Input.get_action_strength("pitch_up"),
@@ -75,30 +67,16 @@ func get_input(delta):
 			Input.get_action_strength("yaw_left") - Input.get_action_strength("yaw_right"),
 			input_response * delta)
 
+	# saggital plane
+	horiz_speed = lerp(horiz_speed,
+			Input.get_action_strength("horiz_left")*50 - Input.get_action_strength("horiz_right")*50,
+			input_response * delta)
+
+	vert_speed = lerp(vert_speed,
+			Input.get_action_strength("vert_up")*50 - Input.get_action_strength("vert_down")*50,
+			input_response * delta)
 	
-	# Godot FPS tutorial
-#	var input_movement_vector = Vector3()
-#
-#	if Input.is_action_pressed("movement_left"):
-#		input_movement_vector.x -=1
-#	if Input.is_action_pressed("movement_right"):
-#		input_movement_vector.x +=1
-#	# Ascend/descend
-#	if Input.is_action_pressed("movement_jump"):
-#		input_movement_vector.z +=1
-#	if Input.is_action_pressed("movement_descend"):
-#		input_movement_vector.z -=1
-#
-#	input_movement_vector = input_movement_vector.normalized()
-
-#	dir = Vector3()
-#	var cam_xform = camera.get_global_transform()
-	# Basis vectors are already normalized
-#	dir += -cam_xform.basis.z * input_movement_vector.y
-#	dir += cam_xform.basis.x * input_movement_vector.x
-#	dir += cam_xform.basis.y * input_movement_vector.z
-		
-
+	
 
 func _physics_process(delta):
 	get_input(delta)
@@ -110,66 +88,10 @@ func _physics_process(delta):
 	transform.basis = transform.basis.orthonormalized()
 
 	if !is_drift:
-		velocity = -transform.basis.z * forward_speed
+		velocity = -transform.basis.z * forward_speed + transform.basis.y * vert_speed + transform.basis.x * horiz_speed
+	
+	
 	
 	$HUD/Panel/Gun_label.text= "Velocity:" + str(-1 * int(forward_speed * 10))
 	
 	move_and_collide(velocity * delta)
-	
-#	process_movement(delta)
-
-
-#func process_movement(delta):
-#	var newVel = vel
-#
-#	var target = dir
-#
-#	target *= MAX_SPEED
-#
-#	newVel = newVel.linear_interpolate(target, calculate_acceleration(newVel) * delta)
-#
-#	vel = move_and_slide(newVel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
-#
-#func calculate_acceleration(newVel):
-#	if dir.dot(newVel) > 0:
-#		return ACCEL
-#	else:
-#		return DEACCELL
-
-
-
-#extends KinematicBody
-#
-#
-
-
-##var MOUSE_SENSITIVITY = 0.05
-#
-#func _ready():
-#	camera = $Rotation_Helper/Camera
-#	rotation_helper = $Rotation_Helper
-#	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-#
-#func _physics_process(delta):
-#	process_input(delta)
-#	process_movement(delta)
-#
-#
-#func process_input(delta):
-#	if Input.is_action_pressed("movement_forward"):
-#		input_movement_vector.y +=1
-#	if Input.is_action_pressed("movement_backward"):
-#		input_movement_vector.y -=1
-#
-#
-#
-#
-#func _input(event):
-#	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-#		rotation_helper.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY))
-#		self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
-#
-#		var camera_rot = rotation_helper.rotation_degrees
-#		camera_rot.x = clamp(camera_rot.x, -70, 70)
-#		rotation_helper.rotation_degrees = camera_rot
