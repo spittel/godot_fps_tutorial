36161e8204002857bb5455d1b45473ee77475135
Jason <theniteowl@gmail.com>
2022-06-19 00:29:03 -0700

    - saggital movement keyboard bound - thrust and retro thrust visual fx

--- Player.gd
+++ Player.gd
@@ -2,8 +2,8 @@ extends KinematicBody
 ##################################
 # new control code
 ##################################
-export var max_speed = 50
-export var acceleration = 0.6
+export var max_speed = 70
+export var acceleration = 0.8
 export var pitch_speed = 1.5
 export var roll_speed = 1.9
 export var yaw_speed = 1.25  # Set lower for linked roll/yaw
@@ -22,6 +22,9 @@ var yaw_input = 0
 var is_drift = false
 
 const SAGGITAL_MULTIPLIER = 50
+const FX_THRUST = .00005
+const FX_RETRO_THRUST = .0005
+const FX_GET_TO_CENTER = .001
 
 var MOUSE_SENSITIVITY = 0.05
 var camera
@@ -30,10 +33,14 @@ var rotation_helper
 var thrusters = preload("res://assets/sounds/main_thrusters.mp3")
 var sagittal = preload("res://assets/sounds/sagittal_thrusters.mp3")
 
+var cam_origin = Vector3()
 
 func _ready():
-	camera = $Rotation_Helper/Camera
 	rotation_helper = $Rotation_Helper
+	
+	var camera_to_center_offset = $Camera_Spatial.translation - $Rotation_Helper.translation
+	
+	cam_origin  = $Rotation_Helper.translation + camera_to_center_offset
 
 
 func get_input(delta):
@@ -62,28 +69,64 @@ func get_input(delta):
 	
 	
 func handle_thrust(delta):
+	is_drift = Input.is_action_pressed("drift")
+
+	if !Input.is_action_pressed("throttle_up") &&  !Input.is_action_pressed("throttle_down"):
+		get_to_center()
+		$MainThrusterSound.stop()
+		return
+
 	if Input.is_action_pressed("throttle_up"):
 		forward_speed = lerp(forward_speed,
-			Input.get_action_strength("throttle_up")*100,
+			Input.get_action_strength("throttle_up") * 100,
 			acceleration * delta)
+		
+		# visual fx
+		if(forward_speed < max_speed):
+			$Camera_Spatial.translate(Vector3(0, 0, forward_speed * FX_THRUST))
 			
+		# sound fx
 		if !$MainThrusterSound.is_playing():
 			$MainThrusterSound.stream = thrusters
 			$MainThrusterSound.play()
-
-	if Input.is_action_just_released("throttle_up"):
-		$MainThrusterSound.stop()
 		
 	if Input.is_action_pressed("throttle_down"):
 		forward_speed = lerp(forward_speed,
 			-Input.get_action_strength("throttle_down")*50,
 			acceleration * delta)
+		#simulate reverse
+		$Camera_Spatial.translate(Vector3(0,0,-FX_RETRO_THRUST))
 			
 	if(forward_speed > max_speed):
+		decel_to_center(FX_GET_TO_CENTER)
 		forward_speed = max_speed
+	
 			
-	is_drift = Input.is_action_pressed("drift")
-		
+
+
+func get_to_center():
+	if($Camera_Spatial.translation.z > cam_origin.z):
+		decel_to_center(FX_GET_TO_CENTER)
+	elif($Camera_Spatial.translation.z < cam_origin.z):
+		accel_to_center(FX_GET_TO_CENTER)
+
+
+func decel_to_center(decel):
+	if($Camera_Spatial.translation != cam_origin):
+		if($Camera_Spatial.translation.z < cam_origin.z):
+			# handle overshooting deceleration
+			$Camera_Spatial.translation = cam_origin
+		else:
+			$Camera_Spatial.translation = $Camera_Spatial.translation + Vector3(0,0,-decel)
+
+func accel_to_center(accel):
+	if($Camera_Spatial.translation != cam_origin):
+		if($Camera_Spatial.translation.z > cam_origin.z):
+			# handle overshooting
+			$Camera_Spatial.translation = cam_origin
+		else:
+			$Camera_Spatial.translation = $Camera_Spatial.translation + Vector3(0,0, accel)
+	
 
 func handle_saggital_thrust(delta):
 	var horiz_input = Input.get_action_strength("horiz_right") * SAGGITAL_MULTIPLIER  - Input.get_action_strength("horiz_left") * SAGGITAL_MULTIPLIER
@@ -112,7 +155,6 @@ func _physics_process(delta):
 
 	if !is_drift:
 		velocity = transform.basis.z * -forward_speed + transform.basis.y * vert_speed + transform.basis.x * horiz_speed
-
 	
 	$HUD/Panel/Gun_label.text= "Velocity:" + str(int(forward_speed))
 	
